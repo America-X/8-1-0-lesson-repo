492a9aa9bce946dc723714a0146280e97335ab53
America.X <164080269+Sleepy-kookoo@users.noreply.github.com>
2024-09-12 12:03:13 -0400

    Initial commit

---
+++ 0-fellow-tracker/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// ES6 module syntax
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import FellowDetails from './pages/FellowDetails';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/fellows/:id" element={<FellowDetails />}></Route>
+    </Routes>
+  )
+}
+
+export default App
---
+++ 1-fellow-tracker-final/frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+import FellowsList from '../components/FellowList';
+import InstructorsList from '../components/InstructorsList';
+const Home = () => {
+  
+
+  return (
+    <>
+      <h1>Home</h1>
+      <main>
+        <FellowsList/>
+        <InstructorsList/>
+      </main>
+    </>
+    
+  )
+}
+
+export default Home;
